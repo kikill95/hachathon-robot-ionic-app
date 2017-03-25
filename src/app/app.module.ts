19275a96ee50208b680f698d4f955008b8fb01b8
light <kirill.gus@gmail.com>
2017-03-25 20:32:48 +0200

    Integrate firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,24 @@ import { HomePage } from '../pages/home/home';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD0kIWL6zCmiDo1l_H_jJEUW03n6AbSjD0",
+  authDomain: "iottestcheb.firebaseapp.com",
+  databaseURL: "https://iottestcheb.firebaseio.com",
+  storageBucket: "iottestcheb.appspot.com",
+  messagingSenderId: "1083520699307"
+};
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { AngularFire } from 'angularfire2';
 
 @Component({
   selector: 'page-home',
@@ -8,23 +8,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   user: string = '';
-  left: number = 0;
-  right: number = 0;
+  motor_l: number = 0;
+  motor_r: number = 0;
   servo: number = 0;
-  constructor(public navCtrl: NavController) {
+  isFired: boolean = false;
+  _db: any;
 
+  constructor(public navCtrl: NavController, public af: AngularFire) {
+    this._db = af.database.object('/');
   }
 
   changeUser(user): void {
-    console.log(user);
+    this.user = user;
   }
 
   onChange(key, value): void {
-    console.log(key, value);
+    if (this.user) {
+      this._db.update({ [this.user + key]: value });
+    }
   }
 
   onFire(): void {
-    console.log('onFire');
+    if (this.user) {
+      this.isFired = !this.isFired;
+      this._db.update({ [this.user + 'fire']: this.isFired ? 1 : 0 });
+    }
   }
 
 }
